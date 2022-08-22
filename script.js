f3fa5666515d4e796a3f12c3166cc422d4c07724
tedtalktimmy <frtntn007@gmail.com>
2022-08-21 22:22:20 -0400

    added series of prompts for password criteria options

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
     // prompted for length of password, choice is between 8 - 12
-    passwordLength = prompt("What password length would you like to generate? (8 - 12)");
+    passwordLength = prompt('What password length would you like to generate? (8 - 12)');
     if (passwordLength < 8 || passwordLength > 128) {
-        alert("password length must be between 8 - 12 characters");
+        alert('password length must be between 8 - 12 characters');
         // confirm input
         return generatePassword();
     }
-}
 // upon clicking button to generate password, user is presented with series of prompts for password criteria
-
 // then criteria is selected by user to be included in password
-
-// prompted for length of password, user chooses length of at least 8 characters and no more than 12.
+if (passwordLength >= 8 && passwordLength <= 12) {
+    var upper = confirm('has uppercase letters?');
+    var lower = confirm('has lowercase letter?');
+    var numbers = confirm('has numbers?');
+    var special = confirm('has special characters?');
+    }
+}
 
 // when asked for character types to include in password, 
 // user confirms whether or not to include lowercase, uppercase, numeric, and/or special characters
