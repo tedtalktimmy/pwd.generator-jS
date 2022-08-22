c65a48fec7bc0dd15a1da3eb3b38ddf19c357a9a
tedtalktimmy <frtntn007@gmail.com>
2022-08-21 22:16:45 -0400

    added instructions on page as comments,

--- script.js
+++ script.js
@@ -2,6 +2,30 @@
 var generateBtn = document.querySelector("#generate");
 
 
+function generatePassword() {
+    // prompted for length of password, choice is between 8 - 12
+    passwordLength = prompt("What password length would you like to generate? (8 - 12)");
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("password length must be between 8 - 12 characters");
+        // confirm input
+        return generatePassword();
+    }
+}
+// upon clicking button to generate password, user is presented with series of prompts for password criteria
+
+// then criteria is selected by user to be included in password
+
+// prompted for length of password, user chooses length of at least 8 characters and no more than 12.
+
+// when asked for character types to include in password, 
+// user confirms whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+// user answers each prompt, input should then be validated and at least one character type selected
+
+// after answering all prompts, password is generated to match criteria
+
+// once password is generated, it should be displayed on page
+
 
 // Add even listener to generate button
 generateBtn.addEventListener("click", writePassword);
